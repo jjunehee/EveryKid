496fc46410995814daca4f9c808bd897e97ac638
choihj00 <add927@kyonggi.ac.kr>
2022-04-04 02:31:13 +0900

    [노수진] 로그인/회원가입 초기 화면 구현

---
+++ app/src/main/java/com/capstone/everykid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.capstone.everykid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }[노수진] 로그인/회원가입 초기 화면 구현
+}
