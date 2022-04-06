52dc642bc70e5fa6803a7cd3db05059f69284435
jjunehee <jjhjjh1159@gamil.com>
2022-04-06 13:47:25 +0900

    profile.xml 추가

--- app/src/main/java/com/capstone/everykid/AccountCreate.java
+++ app/src/main/java/com/capstone/everykid/AccountCreate.java
@@ -1,11 +1,21 @@
 package com.capstone.everykid;
 
+import static android.app.Activity.RESULT_OK;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AccountCreate extends AppCompatActivity {
 
@@ -15,3 +25,4 @@ public class AccountCreate extends AppCompatActivity {
         setContentView(R.layout.create_account);
     }
 }
+
