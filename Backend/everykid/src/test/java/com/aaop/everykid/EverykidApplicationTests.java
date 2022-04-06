ba2355e6ca2d3f223251dadb1967732f7c1b4312
HS-hyeokjin <youngurwls@gmail.com>
2022-04-06 19:19:12 +0900

    spring init

---
+++ Backend/everykid/src/test/java/com/aaop/everykid/EverykidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaop.everykid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverykidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
