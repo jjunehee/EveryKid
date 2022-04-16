6087af194232911f70e91ad8415d4524c7376b8c
HS-hyeokjin <youngurwls@gmail.com>
2022-04-16 12:22:00 +0900

    로그인/로그아웃

---
+++ Backend/everykid/src/main/java/com/aaop/everykid/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.aaop.everykid.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.aaop.everykid.service.ParentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    ParentService parentService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/parents/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("pPHONE")
+                .failureUrl("/members/login/error")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/parents/logout"))
+                .logoutSuccessUrl("/")
+        ;
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(parentService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+
+}
--- Backend/everykid/src/main/java/com/aaop/everykid/service/ParentService.java
+++ Backend/everykid/src/main/java/com/aaop/everykid/service/ParentService.java
@@ -6,13 +6,47 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class ParentService {
+
+public class ParentService implements UserDetailsService {
 
     private final ParentRepository parentRepository;
 
-}
+    public Parent saveParent(Parent parent){
+        validateDuplicateMember(parent);
+        return parentRepository.save(parent);
+    }
 
+    private void validateDuplicateMember(Parent parent){
+        Parent findParent = parentRepository.findByPPHONE(parent.getPPHONE());
+        if(findParent != null){
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String pPHONE) throws UsernameNotFoundException {
+
+        Parent parent = parentRepository.findByPPHONE(pPHONE);
+
+        if(parent == null){
+            throw new UsernameNotFoundException(pPHONE);
+        }
+
+        return User.builder()
+                .username(parent.getPPHONE())
+                .password(parent.getPPWD())
+                .roles(parent.getRole().toString())
+                .build();
+    }
+
+}
 
