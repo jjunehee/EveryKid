232d4b0e63cba106a94653bde594240dbb8f3e3c
HS-hyeokjin <youngurwls@gmail.com>
2022-04-24 02:48:43 +0900

    오류수정

--- Backend/everykid clone/src/main/java/com/aaop/everykid/controller/ParentController.java
+++ Backend/everykid clone/src/main/java/com/aaop/everykid/controller/ParentController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Backend/everykid/src/main/java/com/aaop/everykid/controller/ParentController.java
 package com.aaop.everykid.controller;
 
 import com.aaop.everykid.dto.ParentDto;
@@ -50,53 +49,4 @@ public class ParentController {
         }
         return "실패";
     }
-=======
-package com.aaop.everykid.controller;
-
-import com.aaop.everykid.dto.ParentFormDto;
-import com.aaop.everykid.entity.Parent;
-import com.aaop.everykid.service.LoginService;
-import com.aaop.everykid.service.ParentService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-//import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Slf4j
-@RequestMapping("/parents")
-@RestController
-@RequiredArgsConstructor
-public class ParentController {
-
-    private final ParentService parentService;
-    private final LoginService loginService;
-//    private final PasswordEncoder passwordEncoder;
-
-    @GetMapping(value = "/new")
-    public ParentFormDto getParameter(ParentFormDto pa) {
-
-        return pa;
-    }
-
-    //jpa 문자열
-    @PostMapping(value = "/new")
-    public ParentFormDto parentForm(@RequestBody ParentFormDto parentFormDto){
-        Parent parent = Parent.createParent(parentFormDto);
-        parentService.saveParent(parent);
-        System.out.println("회원가입 시도" + parentFormDto);
-        return parentFormDto;
-    }
-
-    @PostMapping(value = "login")
-    public String loginId(@ModelAttribute Parent parent){
-        System.out.println(parent+"aaaaa");
-        System.out.println(loginService.login(parent));
-        if(loginService.login(parent)){
-            return "성공";
-        }
-        return "실패";
-    }
->>>>>>> b1005988be15d44d4982750d142aa5d96d91e646:Backend/everykid clone/src/main/java/com/aaop/everykid/controller/ParentController.java
 }
--- Backend/everykid clone/src/main/java/com/aaop/everykid/service/LoginService.java
+++ Backend/everykid clone/src/main/java/com/aaop/everykid/service/LoginService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Backend/everykid/src/main/java/com/aaop/everykid/service/LoginService.java
 package com.aaop.everykid.service;
 
 import com.aaop.everykid.entity.Parent;
@@ -24,28 +23,3 @@ public class LoginService  {
         else return true;
     }
 }
-=======
-package com.aaop.everykid.service;
-
-import com.aaop.everykid.entity.Parent;
-import com.aaop.everykid.repository.ParentRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@Transactional
-@RequiredArgsConstructor
-public class LoginService {
-
-    private final ParentRepository parentRepository;
-
-    public boolean login(Parent parent){
-        Parent findParent = parentRepository.findBypID(parent.getPID());
-        if(findParent == null){
-            return false;
-        }
-        return true;
-    }
-}
->>>>>>> b1005988be15d44d4982750d142aa5d96d91e646:Backend/everykid clone/src/main/java/com/aaop/everykid/service/LoginService.java
