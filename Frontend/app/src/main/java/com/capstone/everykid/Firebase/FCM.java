242eeefe45140f4fa52bd0fe3742f5d939eb4e13
jjunehee <jjhjjh1159@gamil.com>
2022-05-20 01:20:10 +0900

    Forground 알림구현

--- Frontend/app/src/main/java/com/capstone/everykid/Firebase/FCM.java
+++ Frontend/app/src/main/java/com/capstone/everykid/Firebase/FCM.java
@@ -1,27 +1,85 @@
 package com.capstone.everykid.Firebase;
 
+import static com.capstone.everykid.Firebase.NotificationClass.NOTIFICATION_ID;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
-import com.google.firebase.messaging.FirebaseMessaging;
+import com.capstone.everykid.R;
+import com.capstone.everykid.View.Activity.MainActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class FCM extends FirebaseMessagingService {
 
     @Override
-    public void onNewToken(@NonNull String token) {
-        super.onNewToken(token);
-        //token을 서버로 전송
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        System.out.println( "From: " + remoteMessage.getFrom());
+
+
+
+        if (remoteMessage.getNotification() != null) {
+            System.out.println("Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+
+//      sendNotification(remoteMessage.getFrom(), remoteMessage.getNotification().getBody());
+        sendNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
     }
 
-    @Override
-    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-        super.onMessageReceived(remoteMessage);
-        //수신한 메시지를 처리
+//    private void sendNotification(String from, String body) {
+//        new Handler(Looper.getMainLooper()).post(new Runnable() {
+//
+//            @Override
+//            public void run() {
+//
+//            }
+//
+//        });
+//    }
+    private void sendNotification(String messageTitle,String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = "My channel ID";
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.ic_class)
+                        .setContentTitle(messageTitle)
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
-
-
-
 }
